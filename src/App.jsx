65c1be0af4c5a433f779f1421cc3e1ae2be26014
Jimmy Huang <jimmymhuang@gmail.com>
2023-03-27 18:14:01 -0500

    imported vite and tailwind packages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <div class="">
+      <Card />
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Jimmy Huang</a>.
+      </div>
+    </div>
+  )
+}
+
+export default App
